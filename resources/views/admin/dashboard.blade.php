e44640924af0e4ebb802939fe873e59e685ce92a
rewtrey <rogorodnyk@gmail.com>
2021-02-18 13:33:54 +0200

    first commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,50 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Categories 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Posts 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">number of visits 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">number of visits today 0</span></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="btn btn-block btn-default" href="#">Create Category</a>
+                <a class="list-group-item" href="#">
+                    <h4 class="list-group-item-heading">Create first Category</h4>
+                    <p class="list-group-item-text">
+                        Number of posts
+                    </p>
+                </a>
+            </div>
+            <div class="col-sm-6">
+                <a class="btn btn-block btn-default" href="#">Create Posts</a>
+                <a class="list-group-item" href="#">
+                    <h4 class="list-group-item-heading">Create first Posts</h4>
+                    <p class="list-group-item-text">
+                        Category
+                    </p>
+                </a>
+            </div>
+        </div>
+    </div>
+
+@endsection
